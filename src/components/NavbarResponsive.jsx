03fe8371a0eecdba7d95bb01cf00173cff3839e8
Joshua Bray <josh.c.bray@gmail.com>
2022-08-11 12:51:50 +0100

    Add bootstrap to default setup

---
+++ src/components/NavbarResponsive.jsx
@@ -0,0 +1,26 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+export default function NavbarResponsive() {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">Renascence</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#welcome">Welcome</Nav.Link>
+            <Nav.Link href="#testamonials">Testamonials</Nav.Link>
+            <Nav.Link href="#howItWorks">How it Works</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#bookConsultation">
+              Book Free Initial Consultation
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
