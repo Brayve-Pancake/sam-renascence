8433cce7b4f1d1c94da685c0a9ed1df85c4c55ae
Joshua Bray <josh.c.bray@gmail.com>
2022-08-13 15:53:34 +0100

    add cloudinary image storage

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,24 @@ import {
   Contact,
   // Footer,
 } from "./components/index";
-import { send } from "emailjs-com";
+
+import { AdvancedImage } from "@cloudinary/react";
+import { Cloudinary } from "@cloudinary/url-gen";
 
 // maybe use star
 
 function App() {
+  const cld = new Cloudinary({
+    cloud: {
+      cloudName: "sam-renascence",
+    },
+  });
+
+  const myImage = cld.image("images/Bunny_mybcnw");
+
   return (
     <div className="App">
+      <AdvancedImage cldImg={myImage} />
       <NavbarResponsive />
       <Welcome />
       <Testamonials />
