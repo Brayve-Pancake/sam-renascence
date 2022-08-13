ac4ecaa73bb8f1c9510e81e98a396000ecebe7f7
Joshua Bray <josh.c.bray@gmail.com>
2022-08-13 17:17:44 +0100

    Add image to background and change bg color

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -4,31 +4,33 @@ import "../scss/welcome.scss";
 
 export default function Welcome() {
   return (
-    <div id="whatWeDo" className="welcome jumptarget">
-      <div>
-        <h1 className="display-6 title1">What we do</h1>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam, sed
-          distinctio, obcaecati non natus eligendi hic sint sapiente quia minus
-          est expedita, nobis ullam! Illo ex voluptatem doloribus sed ad!
-        </p>
+    <div className="backgroundImage">
+      <div id="whatWeDo" className="welcome jumptarget">
+        <div>
+          <h1 className="display-6">What we do</h1>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam, sed
+            distinctio, obcaecati non natus eligendi hic sint sapiente quia
+            minus est expedita, nobis ullam! Illo ex voluptatem doloribus sed
+            ad!
+          </p>
+        </div>
+        <div className="ratio-16x9 ratio">
+          <iframe
+            className="m"
+            src="https://www.youtube.com/embed/vlDzYIIOYmM"
+            title="YouTube video"
+            allowFullScreen
+          ></iframe>
+        </div>
+        <div className="text-center my-2">
+          <Button bsPrefix="contact-btn" variant="dark" size="lg">
+            {/* <Button variant="custom-color" size="lg"> */}
+            Book now
+          </Button>
+        </div>
+        <hr></hr>
       </div>
-      <div className="ratio-16x9 ratio">
-        <iframe
-          className="m"
-          src="https://www.youtube.com/embed/vlDzYIIOYmM"
-          title="YouTube video"
-          allowFullScreen
-        ></iframe>
-      </div>
-
-      <div className="text-center my-2">
-        <Button bsPrefix="contact-btn" variant="dark" size="lg">
-          {/* <Button variant="custom-color" size="lg"> */}
-          Book now
-        </Button>
-      </div>
-      <hr></hr>
     </div>
   );
 }
