901c2dc68f4da6f800e4e0fcaeb659bc169a6086
Joshua Bray <josh.c.bray@gmail.com>
2022-08-16 13:54:56 +0100

    Apply carousel to testamonials

--- src/components/Testamonials.jsx
+++ src/components/Testamonials.jsx
@@ -1,12 +1,119 @@
-import Carousel from "react-bootstrap/Carousel";
+// import Carousel from "react-bootstrap/Carousel";
 import Card from "react-bootstrap/Card";
 import "../scss/testamonials.scss";
+
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 export default function Testamonials() {
   return (
     <div className="testamonials">
       <hr></hr>
-      <Carousel className="testamonials-carousel" interval={null}>
+      <Carousel
+        additionalTransfrom={0}
+        arrows={false}
+        autoPlaySpeed={3000}
+        centerMode={false}
+        className=""
+        dotListClass
+        draggable
+        focusOnSelect={false}
+        infinite
+        itemClass=""
+        keyBoardControl
+        minimumTouchDrag={80}
+        pauseOnHover
+        renderArrowsWhenDisabled={false}
+        renderButtonGroupOutside={false}
+        renderDotsOutside={false}
+        responsive={{
+          mobile: {
+            breakpoint: {
+              max: 576,
+              min: 0,
+            },
+            items: 1,
+            partialVisibilityGutter: 0,
+          },
+          tablet: {
+            breakpoint: {
+              max: 992,
+              min: 576,
+            },
+            items: 2,
+            partialVisibilityGutter: 0,
+          },
+          desktop: {
+            breakpoint: {
+              max: 5000,
+              min: 992,
+            },
+            items: 3,
+            partialVisibilityGutter: 40,
+          },
+        }}
+        rewind={false}
+        rewindWithAnimation={false}
+        rtl={false}
+        shouldResetAutoplay
+        showDots={true}
+        sliderClass=""
+        slidesToSlide={1}
+        swipeable
+      >
+        <div className="carousel-unsplash">
+          <Card bsPrefix="remove-defaults add-flex">
+            <blockquote className="blockquote">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+                posuere erat a ante. ipsum dolor sit amet, consectetur
+                adipiscing elit. Integer posuere erat a ante. Lorem ipsum dolor
+                sit amet, adipiscing elit. Integer posuere a ante. Lorem ipsum
+                dolor sit amet, consectetur adipiscing elit. Integer posuere
+                erat a ante. Lorem ipsum dolor amet, consectetur adipiscing
+                elit. Integer posuere erat a ante.
+              </p>
+              <p className="blockquote-footer">
+                <span className="name">Joshua Bray</span>
+              </p>{" "}
+            </blockquote>
+          </Card>
+        </div>
+        <div className="carousel-unsplash">
+          <Card bsPrefix="remove-defaults add-flex">
+            <blockquote className="blockquote">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+                posuere erat a ante. ipsum dolor sit amet, consectetur
+                adipiscing elit. Integer posuere erat a ante. Lorem ipsum dolor
+                sit amet, adipiscing elit.
+              </p>
+              <p className="blockquote-footer">
+                <span className="name">Joshua Bray</span>
+              </p>{" "}
+            </blockquote>
+          </Card>
+        </div>
+        <div className="carousel-unsplash">
+          <Card bsPrefix="remove-defaults add-flex">
+            <blockquote className="blockquote">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+                posuere erat a ante. ipsum dolor sit amet, consectetur
+                adipiscing elit. Integer posuere erat a ante. Lorem ipsum dolor
+                sit amet, adipiscing elit. Integer posuere a ante. Lorem ipsum
+                dolor sit amet, consectetur adipiscing elit. Integer posuere
+                erat a ante. Lorem ipsum dolor amet, consectetur adipiscing
+                elit. Integer posuere erat a ante.
+              </p>
+              <p className="blockquote-footer">
+                <span className="name">Joshua Bray</span>
+              </p>{" "}
+            </blockquote>
+          </Card>
+        </div>
+      </Carousel>
+      {/* <Carousel className="testamonials-carousel" interval={null}>
         <Carousel.Item>
           <Card bsPrefix="remove-defaults add-flex">
             <blockquote className="blockquote">
@@ -58,7 +165,7 @@ export default function Testamonials() {
             </blockquote>
           </Card>
         </Carousel.Item>
-      </Carousel>
+      </Carousel> */}
       <hr></hr>
     </div>
   );
