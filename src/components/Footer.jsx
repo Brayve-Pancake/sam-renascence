e609a7678c64c390b9d0b846ec0202d95a00af67
Joshua Bray <josh.c.bray@gmail.com>
2022-08-16 08:48:02 +0100

    Add Footer contents

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,11 @@ export default function Footer() {
   return (
     <div className="footer">
       <hr></hr>
+      <div className="footer-text">
+        Website Development by{" "}
+        <a href="https://github.com/Brayve-Pancake">BRAY</a>
+      </div>
+      <br></br>
     </div>
   );
 }
